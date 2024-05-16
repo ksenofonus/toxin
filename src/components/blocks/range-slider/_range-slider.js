ccec708bfe39b3d992f7d20d91b698f965872fed
ksenofonus <danilova.ka88@yandex.ru>
2024-05-16 11:57:12 +0400

    feat: impement select price range with inputs fields

--- src/components/blocks/range-slider/_range-slider.js
+++ src/components/blocks/range-slider/_range-slider.js
@@ -1,16 +1,42 @@
 import '../../../assets/lib/noUiSlider-15.7.2/_nouislider.scss';
 import * as noUiSlider from '../../../assets/lib/noUiSlider-15.7.2/_nouislider';
+import * as wNumb from '../../../assets/lib/wNumb';
 
-const slider = document.querySelector('.range-slider_slider');
+const sliderFormat = document.querySelector('.range-slider_slider');
 
-noUiSlider.create(slider, {
+noUiSlider.create(sliderFormat, {
     start: [5000, 10000],
     connect: true,
-    step: 100,
+    step: 50,
     range: {
-        'min': [0],
+        'min': [500],
         'max': [15000]
-    }
+    },
+    format: wNumb({
+        decimals: 0,
+        thousand: ' ',
+        suffix: '₽'
+    })
+});
+
+const minPrice = document.querySelector('.min-price');
+const maxPrice = document.querySelector('.max-price');
+
+const inputFormat = [
+    minPrice,
+    maxPrice
+];
+
+
+sliderFormat.noUiSlider.on('update', function (values, handle) {
+    inputFormat[handle].value = values[handle];
+});
+
+minPrice.addEventListener('change', function () {
+    sliderFormat.noUiSlider.set(this.value);
+});
+maxPrice.addEventListener('change', function () {
+    sliderFormat.noUiSlider.set(this.value);
 });
 
 export default create;
--- src/pages/uikit/uikit.js
+++ src/pages/uikit/uikit.js
@@ -1,6 +1,6 @@
 import '../uikit/uikit.scss';
 import {likeToggle} from '../../components/blocks/buttons/like/_like';
 import create from '../../components/blocks/range-slider/_range-slider';
+import '../../components/blocks/range-slider/_range-slider'
 likeToggle();
-create();
 
