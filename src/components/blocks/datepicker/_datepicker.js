6a322245bfceb2e2bed833a6922d768ca389a53c
ksenofonus <danilova.ka88@yandex.ru>
2024-05-20 21:51:30 +0400

    feat: add filter date dropdown field

--- src/components/blocks/datepicker/_datepicker.js
+++ src/components/blocks/datepicker/_datepicker.js
@@ -2,4 +2,12 @@ import AirDatepicker from 'air-datepicker';
 import 'air-datepicker/air-datepicker.css';
 
 const datepicker = document.querySelectorAll('.datepicker');
-datepicker.forEach((item) => new AirDatepicker(item))
+datepicker.forEach((item) => new AirDatepicker(item));
+
+const filterDatepicker = document.querySelector('.filter-datepicker');
+new AirDatepicker(filterDatepicker, {
+  range: true,
+  multipleDatesSeparator: ' - ',
+  dateFormat: 'dd MMM'
+  }
+  );
