5a2f6bc06b9ad12ca69375fe1b847d3111371dfb
ksenofonus <danilova.ka88@yandex.ru>
2024-05-20 20:31:50 +0400

    fix: fix date dropdown

--- src/components/blocks/datepicker/_datepicker.js
+++ src/components/blocks/datepicker/_datepicker.js
@@ -1,4 +1,5 @@
 import AirDatepicker from 'air-datepicker';
 import 'air-datepicker/air-datepicker.css';
 
-new AirDatepicker('.datepicker');
+const datepicker = document.querySelectorAll('.datepicker');
+datepicker.forEach((item) => new AirDatepicker(item))
