b16903864226d4ae515174d75b2422656ff3ca93
ksenofonus <danilova.ka88@yandex.ru>
2024-05-22 15:37:28 +0400

    feat: add togglу active class on + / - buttons

--- src/components/blocks/dropdown/_dropdown.js
+++ src/components/blocks/dropdown/_dropdown.js
@@ -1,35 +1,63 @@
-const dropdownContainer = document.querySelectorAll('.item-dropdown');
+// const dropdownContainer = document.querySelectorAll('.item-dropdown');
 const dropdownSelect = document.querySelectorAll('.item-dropdown_selection');
-const dropdownMenu = document.querySelectorAll('.item-dropdown_menu');
-const increaseButton = document.querySelectorAll('.increase');
-const decreaseButton = document.querySelectorAll('.decrease');
-const itemQuantity = document.querySelectorAll('.item-quantity');
+// const dropdownMenu = document.querySelectorAll('.item-dropdown_menu');
 
-//показать меню / show menu
-dropdownSelect.forEach((item, index) => {
+// const decreaseButton = document.querySelectorAll('.decrease');
+// const itemQuantity = document.querySelectorAll('.item-quantity');
+
+
+dropdownSelect.forEach((item) => {
   item.addEventListener('click', () => {
-    item.nextElementSibling.classList.toggle('item-dropdown_menu__closed');
-    item.parentNode.classList.toggle('item-dropdown__opened');
-  })
-})
-//кнопка плюс / increase button
-increaseButton.forEach((item, index) => {
-  item.addEventListener('click', (event) => {
-    event.preventDefault();
-    let itemVal = parseInt(itemQuantity[index].textContent);
-    itemVal < 20 ? itemQuantity[index].textContent = itemVal + 1 : itemVal = 20;
-    
-  })
-})
-//кнопка минус / decrease button
-decreaseButton.forEach((item, index) => {
-  item.addEventListener('click', (event) => {
-    event.preventDefault();
-    let itemVal = parseInt(itemQuantity[index].textContent);
-    itemVal > 0 ? itemQuantity[index].textContent = itemVal - 1 : itemVal = 0;
+    const dropdownContainer = item.parentNode;
+    const dropdownMenu = item.nextElementSibling;
+    const increaseButton = dropdownMenu.querySelectorAll('.increase');
+    const decreaseButton = dropdownMenu.querySelectorAll('.decrease');
+    const itemQuantity = dropdownMenu.querySelectorAll('.item-quantity');
+    const limitGuest = 20;
+    //показать меню / show menu
+    dropdownMenu.classList.toggle('item-dropdown_menu__closed');
+    dropdownContainer.classList.toggle('item-dropdown__opened');
+    //кнопка плюс
+    increaseButton.forEach((item, index) => {
+      item.addEventListener('click', (event) => {
+        event.preventDefault();
+        let itemVal = parseInt(itemQuantity[index].textContent);
+        itemVal < limitGuest ? itemQuantity[index].textContent = itemVal + 1 : itemVal = limitGuest;
+        if (itemVal >= 0) {
+          decreaseButton[index].classList.remove('dropdown-button__inactive');
+        }
+        if (itemVal >= limitGuest - 1) {
+          item.classList.add('dropdown-button__inactive');
+        }
+      })
+    })
+    //кнопка минус
+    decreaseButton.forEach((item, index) => {
+      item.addEventListener('click', (event) => {
+        event.preventDefault();
+        let itemVal = parseInt(itemQuantity[index].textContent);
+        itemVal > 0 ? itemQuantity[index].textContent = itemVal - 1 : itemVal = 0;
+        if (itemVal <= limitGuest) {
+          increaseButton[index].classList.remove('dropdown-button__inactive');
+        }
+        if (itemVal <= 1) {
+          item.classList.add('dropdown-button__inactive');
+        }
+      })
+    })
   })
 })
 
+const setActiveButton = (x, index) => {
+  if (x === 0) {
+    decreaseButton[index].classList.add('dropdown-button__inactive');
+  } else if (x > 0 && x < 20) {
+    decreaseButton.classList.remove('dropdown-button__inactive');
+    increaseButton.classList.remove('dropdown-button__inactive');
+  } else if (x === 20) {
+    increaseButton.classList.add('dropdown-button__inactive');
+  }
+}
 //установить текст / set selection text
 const setSelectionTextGuest = (item, index) => {
   item.textContent = sumTotal()
