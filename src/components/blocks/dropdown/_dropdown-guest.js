d401e5672d712ce63229d2233076d3841c7090fe
ksenofonus <danilova.ka88@yandex.ru>
2024-05-23 22:55:00 +0400

    refactor: refactor form page layout

--- src/components/blocks/dropdown/_dropdown-guest.js
+++ src/components/blocks/dropdown/_dropdown-guest.js
@@ -1,10 +1,10 @@
 import { dropdown } from './_dropdown';
-import { sumTotal } from './_dropdown';
 
-const dropdownSelect = document.getElementById('guestdrop');
+const dropdownSelect = document.querySelector('#guestdrop .item-dropdown__selection');
+const dropdownSelectOpen = document.querySelector('#guestopen .item-dropdown__selection');
 const dropdownContainer = dropdownSelect.parentNode;
-const itemQuantity = dropdownContainer.querySelectorAll('.item-quantity')
 const clear = dropdownContainer.querySelector('.dropdown-btn__clear');
 const apply = dropdownContainer.querySelector('.dropdown-btn__apply');
 dropdown(dropdownSelect, clear, apply);
+// dropdown(dropdownSelectOpen, clear, apply);
 
