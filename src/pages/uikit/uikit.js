658710714db242f2b0c057f309c0bdcbf7228a4f
ksenofonus <danilova.ka88@yandex.ru>
2024-06-05 12:15:03 +0400

    feat: add room form

--- src/pages/uikit/uikit.js
+++ src/pages/uikit/uikit.js
@@ -7,6 +7,7 @@ import '../../components/blocks/datepicker/_datepicker';
 import '../../components/blocks/forms/masked-text/_mask';
 import '../../components/blocks/buttons/checkbox/expandable/_expandable';
 import '../../components/blocks/forms/search-form/_search-form';
+import '../../components/blocks/forms/room/_room';
 
 
 
