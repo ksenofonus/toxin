e67ec505955e53cd2b610b332f910a104fdd387a
ksenofonus <danilova.ka88@yandex.ru>
2024-06-05 15:41:05 +0400

    feat: start room block layout

--- src/pages/uikit/uikit.js
+++ src/pages/uikit/uikit.js
@@ -7,7 +7,7 @@ import '../../components/blocks/datepicker/_datepicker';
 import '../../components/blocks/forms/masked-text/_mask';
 import '../../components/blocks/buttons/checkbox/expandable/_expandable';
 import '../../components/blocks/forms/search-form/_search-form';
-import '../../components/blocks/forms/room/_room';
+import '../../components/blocks/forms/room-search/_room-search';
 
 
 
