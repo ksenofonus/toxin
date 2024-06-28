f65bbc46c61de51429b0ba5a2b44a97a83a61ecf
ksenofonus <danilova.ka88@yandex.ru>
2024-06-28 10:40:38 +0400

    feat: add index.js index.scss

---
+++ src/pages/index/index.js
@@ -0,0 +1,10 @@
+import '../index/index.scss';
+import '../../components/blocks/buttons/like/_like';
+import '../../components/blocks/range-slider/_range-slider';
+import '../../components/blocks/pagination/_paginat';
+import '../../components/blocks/dropdown/_dropdown-guest';
+import '../../components/blocks/datepicker/_datepicker';
+import '../../components/blocks/forms/masked-text/_mask';
+import '../../components/blocks/buttons/checkbox/expandable/_expandable';
+import '../../components/blocks/forms/search-form/_search-form';
+import '../../components/blocks/forms/room-search/_room-search';
