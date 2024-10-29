309a1583def259f4be9f4b48628227f0c3911146
ksenofonus <danilova.ka88@yandex.ru>
2024-10-29 16:13:33 +0400

    fix: fis range style on -range-from- day, start datepicker function

--- src/components/blocks/datepicker/_datepicker.js
+++ src/components/blocks/datepicker/_datepicker.js
@@ -10,38 +10,44 @@ const filterDatepicker = document.querySelector('.filter-datepicker');
 new AirDatepicker(filterDatepicker, {
   range: true,
   multipleDatesSeparator: ' - ',
-  dateFormat: 'dd MMM'
-  }
-  );
-
-let datepickerStart = document.getElementById('start-date');
-let datepickerEnd = document.getElementById('end-date');
-
+  dateFormat: 'dd MMM',
+});
+//datepicker fields
+let startDate = document.getElementById('start-date');
+let endDate = document.getElementById('end-date');
+//button
 let apply = {
   content: 'Применить',
   className: 'apply-button',
-  onclick: (dp) => {
-    
-  }
-}
+  onclick: (dp) => {},
+};
 
-let start = new AirDatepicker(datepickerStart, {
+
+
+let start = new AirDatepicker(startDate, {
   range: true,
   onSelect({ date }) {
+    console.log(date)
     end.update({
-      minDate: date,
-    });
+      minDate: date[0],
+      maxDate: date[1],
+      selectedDates: [date]
+    })
+    end.selectDate(date[1])
+    startDate.value = start.formatDate(date[0], 'dd.MM.yyyy');
   },
   buttons: ['clear', apply],
-  
 });
 
-let end = new AirDatepicker(datepickerEnd, {
+
+let end = new AirDatepicker(endDate, {
+  range: true,
+  position: 'bottom right',
   onSelect({ date }) {
     start.update({
-      maxDate: date,
+      minDate: date[0],
+      maxDate: date[1]
     });
   },
-  buttons: ['clear']
+  buttons: ['clear'],
 });
-start.show();
