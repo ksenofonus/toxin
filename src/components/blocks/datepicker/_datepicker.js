ee169e0fd510afddc303d7366ffaba2dbf08f26b
ksenofonus <danilova.ka88@yandex.ru>
2024-10-30 13:52:55 +0400

    fix: fix datepicker with one inicialisation

--- src/components/blocks/datepicker/_datepicker.js
+++ src/components/blocks/datepicker/_datepicker.js
@@ -13,36 +13,57 @@ new AirDatepicker(filterDatepicker, {
   dateFormat: 'dd MMM',
 });
 //datepicker fields
-let startDate = document.getElementById('start-date');
-let endDate = document.getElementById('end-date');
+const startDate = document.getElementById('start-date');
+const endDate = document.getElementById('end-date');
 //button
-let apply = {
+const apply = {
   content: 'Применить',
   className: 'apply-button',
   onclick: (dp) => {},
 };
 
-
-
-let start = new AirDatepicker(startDate, {
+const datepicker = new AirDatepicker(startDate, {
   range: true,
-  onSelect({ date }) {
-    end.update({
-      selectedDates: [date]
-    })
-    
-  },
   buttons: ['clear', apply],
-});
-
-
-let end = new AirDatepicker(endDate, {
-  range: true,
-  position: 'bottom right',
-  onSelect({ date }) {
-    start.unselectDate();
-    
-    
+  onSelect: ({ date }) => {
+    console.log(date[0], date[1]);
+    datepicker.$el.value = endDate.value = datepicker.formatDate(
+      date[0],
+      'dd.MM.yyyy',
+    );
+    if (date.length > 1) {
+      endDate.value = datepicker.formatDate(date[1], 'dd.MM.yyyy');
+    }
   },
-  buttons: ['clear'],
 });
+
+endDate.addEventListener('click', () => datepicker.show())
+
+document.body.addEventListener('click', (e) => {
+  const target = e.target;
+  const isDatepicker =
+    target === datepicker.$datepicker || datepicker.$datepicker.contains(target);
+  const isStartInput =
+    target === datepicker.$el || datepicker.$el.contains(target);
+  let isEndInput = target === endDate || endDate.contains(target)
+  if (!isDatepicker && !isStartInput && !isEndInput) {
+    datepicker.hide();
+  }
+})
+
+
+
+// let end = new AirDatepicker(endDate, {
+//   range: true,
+//   position: 'bottom right',
+//   onSelect({ date }) {
+//     // start.update({
+//     //   viewDate: end.viewDate,
+//     //   selectedDates: [date],
+//     // }),
+//     //   {
+//     //     silent: true,
+//     //   };
+//   },
+//   buttons: ['clear'],
+// });
