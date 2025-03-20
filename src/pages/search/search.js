31d3101782e1f9041472f48cda213b636f9c1f06
ksenofonus <danilova.ka88@yandex.ru>
2025-03-20 19:35:20 +0400

    refactor: rooms on room search page

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -4,10 +4,13 @@ import '../../components/blocks/dropdown/_dropdown-guest';
 import '../../components/blocks/range-slider/_range-slider';
 import '../../components/blocks/buttons/checkbox/expandable/_expandable';
 import template from '../../components/blocks/room/_room.pug';
-import { list } from './_roomslist';
-
+import Data from '../../assets/json/rooms.json';
+const rooms = Data.reduce((acc, currentdata, index) => {
+  acc[index] = currentdata;
+  return acc;
+}, {});
 const roomsContainer = document.querySelector('.room-wrapper');
-for (let key in list) {
-  roomsContainer.insertAdjacentHTML('afterbegin', template(list[key]));
+for (let key in rooms) {
+  roomsContainer.insertAdjacentHTML('beforeend', template(rooms[key]));
 }
 
