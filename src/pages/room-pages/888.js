d3fd4ccb575561b63d57a0c9b339c7c18b7f1cc1
ksenofonus <danilova.ka88@yandex.ru>
2025-05-15 15:23:17 +0400

    refactor: chart legend

--- src/pages/room-pages/888.js
+++ src/pages/room-pages/888.js
@@ -12,28 +12,123 @@ const data = [
   { magnificently: 130, good: 65, satisfactorily: 65, disappointed: 0},
 ];
 const doughnut = document.getElementById('myChart');
-const legend = document.getElementById('legend');
-const ctx = doughnut.getContext('2d');
-const gradient = ctx.createLinearGradient(20, 0, 220, 0);
 
+const ctx = doughnut.getContext('2d');
+const gradientPerfect = ctx.createLinearGradient(0, 0, 0, 120);
+gradientPerfect.addColorStop(0, "#FFE39C");
+gradientPerfect.addColorStop(1, '#FFBA9C');
+const gradientGood = ctx.createLinearGradient(0, 0, 0, 120);
+gradientGood.addColorStop(0, '#6fcf97');
+gradientGood.addColorStop(1, '#66d2ea');
+const gradientSatisfate = ctx.createLinearGradient(0, 0, 0, 120);
+gradientSatisfate.addColorStop(0, '#bc9cff');
+gradientSatisfate.addColorStop(1, '#8ba4f9');
+const gradientDissapoint = ctx.createLinearGradient(0, 0, 0, 120);
+gradientDissapoint.addColorStop(0, '#909090');
+gradientDissapoint.addColorStop(1, '#3D4975');
+
+const getOrCreateLegendList = (chart, id) => {
+  const legendContainer = document.getElementById(id);
+  let listContainer = legendContainer.querySelector('ul');
+
+  if (!listContainer) {
+    listContainer = document.createElement('ul');
+    listContainer.style.display = 'flex';
+    listContainer.style.flexDirection = 'column';
+    listContainer.style.margin = 0;
+    listContainer.style.padding = 0;
+
+    legendContainer.appendChild(listContainer);
+  }
+
+  return listContainer;
+};
+const htmlLegendPlugin = {
+  id: 'htmlLegend',
+  afterUpdate(chart, args, options) {
+    const ul = getOrCreateLegendList(chart, options.containerID);
+
+    // Remove old legend items
+    while (ul.firstChild) {
+      ul.firstChild.remove();
+    }
+
+    // Reuse the built-in legendItems generator
+    const items = chart.options.plugins.legend.labels.generateLabels(chart);
+    const labelClass = ['perfect', 'good', 'satisfact', 'disappoint'];
+
+    items.forEach((item, index) => {
+      const li = document.createElement('li');
+      li.style.cursor = 'pointer';
+      li.style.display = 'flex';
+      li.style.alignItems = 'center';
+      li.onclick = () => {
+        const { type } = chart.config;
+        if (type === 'pie' || type === 'doughnut') {
+          // Pie and doughnut charts only have a single dataset and visibility is per item
+          chart.toggleDataVisibility(item.index);
+        } else {
+          chart.setDatasetVisibility(
+            item.datasetIndex,
+            !chart.isDatasetVisible(item.datasetIndex)
+          );
+        }
+        chart.update();
+      };
+
+      // Color box
+      const boxSpan = document.createElement('span');
+      boxSpan.classList.add(labelClass[index]);
+      boxSpan.style.display = 'inline-block';
+      boxSpan.style.flexShrink = 0;
+      boxSpan.style.height = '10px';
+      boxSpan.style.marginRight = '10px';
+      boxSpan.style.width = '10px';
+      boxSpan.style.borderRadius = '100%';
+
+      // Text
+      const textContainer = document.createElement('p');
+      textContainer.style.color = item.fontColor;
+      textContainer.style.margin = 0;
+      textContainer.style.padding = 0;
+      textContainer.style.textDecoration = item.hidden ? 'line-through' : '';
+
+      const text = document.createTextNode(item.text);
+      textContainer.appendChild(text);
+
+      li.appendChild(boxSpan);
+      li.appendChild(textContainer);
+      ul.appendChild(li);
+    });
+  },
+};
 new Chart(doughnut, {
   type: 'doughnut',
   data: {
     datasets: [
       {
         data: [130, 65, 65, 0],
-        // backgroundColor: [],
+        backgroundColor: [
+          gradientPerfect,
+          gradientSatisfate,
+          gradientGood,
+          gradientDissapoint,
+        ],
       },
     ],
-    labels: ['Великолепно', 'Хорошо', 'Удовлетворительно', 'Разочарован'],
+    labels: ['Великолепно', 'Удовлетворительно', 'Хорошо', 'Разочарован'],
   },
   options: {
     rotation: 180,
     cutout: '90%',
     plugins: {
+      htmlLegend: {
+        containerID: 'legend',
+      },
       legend: {
-        display: false
+        display: false,
       },
     },
   },
+  plugins: [htmlLegendPlugin],
 });
