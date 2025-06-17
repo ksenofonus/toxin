ab91da89b2f13e78d656119591cd0f83a5878115
ksenofonus <danilova.ka88@yandex.ru>
2025-06-17 22:31:35 +0400

    fix: change background images and their positions on landing page

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,6 +1,6 @@
 import './search.scss';
-import '../../components/blocks/pagination/_paginat';
-import '../../components/blocks/datepicker/_filterdate';
-import '../../components/blocks/dropdown/_dropdown-guest';
-import '../../components/blocks/range-slider/_range-slider';
-import '../../components/blocks/buttons/checkbox/expandable/_expandable';
+import 'Components/blocks/pagination/_paginat';
+import 'Components/blocks/datepicker/_filterdate';
+import 'Components/blocks/dropdown/_dropdown-guest';
+import 'Components/blocks/range-slider/_range-slider';
+import 'Components/blocks/buttons/checkbox/expandable/_expandable';
